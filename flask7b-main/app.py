8fd695202168dd8d07612bd076883290d5670bf2
buda2103 <garciarobledodavid18@gmail.com>
2024-09-18 17:53:42 -0500

    Update app.py

--- flask7b-main/app.py
+++ flask7b-main/app.py
@@ -3,11 +3,16 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pusher
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("app.html")
+ @app.route("/")
+def index():
+    return render_template("Pago_de_curso.html")
 
 @app.route("/alumnos")
 def alumnos():
@@ -18,3 +23,12 @@ def alumnosGuardar():
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
+
+def evento ():
+ pusher_client = pusher.Pusher(
+app_id = "1867163"
+key = "2358693f2b619b363f59"
+secret = "880f60b50e86e4555c43"
+cluster = "us2"
+)
+pusher_client.trigger("my-channel", "my-event", {})
